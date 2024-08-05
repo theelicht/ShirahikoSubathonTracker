2f4852696c49a1619b94d656346966114359044d
theelicht <melleeluc@gmail.com>
2024-08-05 22:18:48 +0200

    Add additional player information

--- ShiraSubathonTracker.DAL/Entities/Minecraft/MinecraftPlayer.cs
+++ ShiraSubathonTracker.DAL/Entities/Minecraft/MinecraftPlayer.cs
@@ -15,4 +15,11 @@ public class MinecraftPlayer
     
     [MaxLength(50)]
     public required string PlayerName { get; set; }
+    
+    [MaxLength(50)]
+    public required string Uuid { get; set; }
+    
+    public int MinutesOnline { get; set; }
+    
+    public DateTimeOffset LastOnline { get; set; }
 }
